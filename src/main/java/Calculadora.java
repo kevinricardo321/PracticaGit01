e3ee408f8b496a2754efbc48ff94753698a6d297
Kevin <20253ds131@utez.edu.mx>
2026-05-25 23:40:37 -0600

    new metod suma

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,4 +1,19 @@
 package PACKAGE_NAME;
 
 public class Calculadora {
+
+    private double numero1;
+    private double numero2;
+
+    public Calculadora(){}
+
+    public Calculadora(double numero1 , double numero2){
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public double sumar(){
+        return numero1+numero2;
+    }
+
 }
